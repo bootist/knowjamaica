2580bf8f93574f699a08142bde7587325e2bf43e
bootist <55961162+bootist@users.noreply.github.com>
2019-12-09 00:21:25 -0500

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,8 +3,9 @@ include_once ('includes/header.php'); ?>
 
 <div class="jumbotron jumbotron-fluid">
 <div class="container">
-    <h1 class="display-4">Don't contact us we will contact you!</h1>
-    <p class="lead">We simply cannot be contacted</p>
+    <h1 class="display-4">20 Main Street, kingston 5</h1>
+    <p class="lead">1876-312-4455</p>
+    <p class="lead">customerservice@knowja.com</p>
   </div>
 </div>
 
